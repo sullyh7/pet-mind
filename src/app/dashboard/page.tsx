1eefb70712272e4dbdfb9be250d1eca1c1c3113b
sulayman <ec22675@qmul.ac.uk>
2024-04-02 18:26:23 +0100

    added dashboard sketchup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { createClient } from '../../../utils/supabase/server'
+import BookingsView from '@/components/BookingsView';
 
-const Dashboard = () => {
+const Dashboard = async () => {
+    const client = createClient();
+    const user = await client.auth.getUser()
+    const profile = await client.from("profiles").select("*").single();
+
+    if (!user.data.user || !profile.data) {
+        return <h1>Please sign in.</h1>
+    }
   return (
-    <div>Dashboard</div>
+    <div>
+        <h1 className='font-bold text-4xl sm:text-5xl'>Welcome back <span className='text-[#DB3066]'>{profile.data.first_name}</span></h1>
+        <BookingsView/>
+    </div>
   )
 }
 
--- utils/supabase/types/supabase.ts
+++ utils/supabase/types/supabase.ts
@@ -9,7 +9,35 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      [_ in never]: never
+      profiles: {
+        Row: {
+          first_name: string | null
+          id: string
+          last_name: string | null
+          updated_at: string | null
+        }
+        Insert: {
+          first_name?: string | null
+          id: string
+          last_name?: string | null
+          updated_at?: string | null
+        }
+        Update: {
+          first_name?: string | null
+          id?: string
+          last_name?: string | null
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "profiles_id_fkey"
+            columns: ["id"]
+            isOneToOne: true
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
     }
     Views: {
       [_ in never]: never
